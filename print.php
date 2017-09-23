e7fafad18e14455315362bf0488b89af859a49f0
rahulkumargarg2017 <32137533+rahulkumargarg2017@users.noreply.github.com>
2017-09-23 11:21:40 +0530

    print.php

---
+++ print.php
@@ -0,0 +1,27 @@
+<?php
+//Login details
+$servername = "mysql5.gear.host";
+$username = "sensordata2017";
+$password = "Mc090LF!0e~2";
+$dbname = "sensordata2017";
+//Create connection
+$conn = new mysqli("$servername", $username, $password, $dbname);
+//Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+echo"Yes";
+$sql = "SELECT * FROM sensorst";
+    $result = mysqli_query($conn ,$sql);
+    $array = [];
+    
+    while($row = $result->fetch_assoc()){
+        $array[] = $row;
+    }
+    
+    header('Content-Type:Application/json');
+    echo json_encode($array);
+    mysqli_free_result($result);
+//Getting all data from server
+?>
