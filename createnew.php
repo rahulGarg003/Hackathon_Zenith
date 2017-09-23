be85568b198700cffa1fd72bedf881a4494108bc
rahulkumargarg2017 <32137533+rahulkumargarg2017@users.noreply.github.com>
2017-09-24 01:37:03 +0530

    createnew.php

---
+++ createnew.php
@@ -0,0 +1,21 @@
+<?php
+//Login details
+$servername = "mysql5.gear.host";
+$username = "sensordata2017";
+$password = "Mc090LF!0e~2";
+$dbname = "sensordata2017";
+//Create connection
+$conn = new mysqli("$servername", $username, $password, $dbname);
+//Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+//Create table query
+$sql = "CREATE TABLE sensordatatable(timestamp Varchar(20),AmmoniaLevel Varchar(10),OxygenLevel Varchar(10),WaterLevel Varchar(8), Location Varchar(50))";
+if ($conn->query($sql) === TRUE) {
+    echo "Table blogs created successfully!";} 
+else {
+    echo "Error creating table: " . $conn->error;
+}
+?>
